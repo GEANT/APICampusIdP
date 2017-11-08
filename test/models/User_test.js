7f432f59e89fd6fe3b083482d2388d7130f8c0d5
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2017-11-08 23:17:39 +0000

    update user_test

--- test/models/User_test.js
+++ test/models/User_test.js
@@ -1,5 +1,7 @@
 const assert = require("assert");
 const request = require("supertest");
+const chai = require('chai');
+const expect = chai.expect;
 const app = require("../../app");
 const mongoose = require("mongoose");
 const User = require("../../models/User");
@@ -7,119 +9,180 @@ const sampleUser = require("../sample_data").sampleUser;
 const sampleDisabledUser = require("../sample_data").sampleDisabledUser;
 
 describe("Model User", () => {
-  before(done => {
-    User.find().remove(done);
-  });
-
-  before(done => {
-    mongoose.connection.collections.users.drop((err, res) => {
-      if (err) {
-        done(err);
-      } else {
-        done();
-      }
+    before(done => {
+        User.find().remove(done);
     });
-  });
 
-  describe("Create", () => {
-    it("create user as disabled", done => {
-      let user = new User(sampleDisabledUser);
-      user
-        .save()
-        .then(() => {
-          assert(!user.isNew);
-          done();
-        })
-        .catch(err => {
-          done(err);
+    before(done => {
+        mongoose.connection.collections.users.drop((err, res) => {
+            if (err) {
+                done(err);
+            } else {
+                done();
+            }
         });
     });
 
-    it("read created user", done => {
-      let sampleUser = sampleDisabledUser;
-      let user = User.findOne({ username: sampleUser.username })
-        .then(users => {
-          assert(
-            users.username === sampleUser.username,
-            "username dos not match"
-          );
-          assert(users.email === sampleUser.email, "Email does not match");
-          // console.log(`FFF: ${JSON.stringify(users)}`);
-          assert(users.enabled === false);
-          users.checkPassword(sampleUser.password, function(err, res) {
-            assert(res === true, "Password does not match");
-          });
-          done();
-        })
-        .catch(err => {
-          done(err);
+    describe("Create", () => {
+        it("create user as disabled", done => {
+            let user = new User(sampleDisabledUser);
+            user
+                .save()
+                .then(() => {
+                    assert(!user.isNew);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
+        });
+
+        it("read created user", done => {
+            let sampleUser = sampleDisabledUser;
+            let user = User.findOne({username: sampleUser.username})
+                .then(users => {
+                    assert(
+                        users.username === sampleUser.username,
+                        "username dos not match"
+                    );
+                    assert(users.email === sampleUser.email, "Email does not match");
+                    // console.log(`FFF: ${JSON.stringify(users)}`);
+                    assert(users.enabled === false);
+                    users.checkPassword(sampleUser.password, function (err, res) {
+                        assert(res === true, "Password does not match");
+                    });
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
+        });
+
+        it("create user as enabled", done => {
+            let user = new User(sampleUser);
+            user
+                .save()
+                .then(() => {
+                    assert(!user.isNew);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
+        });
+
+        it("read created user", done => {
+            let user = User.findOne({username: sampleUser.username})
+                .then(users => {
+                    assert(
+                        users.username === sampleUser.username,
+                        "username dos not match"
+                    );
+                    assert(users.email === sampleUser.email, "Email does not match");
+                    // console.log(`FFF: ${JSON.stringify(users)}`);
+                    assert(users.enabled === true);
+                    users.checkPassword(sampleUser.password, function (err, res) {
+                        assert(res === true, "Password does not match");
+                    });
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
-    it("create user as enabled", done => {
-      let user = new User(sampleUser);
-      user
-        .save()
-        .then(() => {
-          assert(!user.isNew);
-          done();
-        })
-        .catch(err => {
-          done(err);
+    describe("Remove", () => {
+        beforeEach(done => {
+            var nuser = new User(sampleUser);
+            nuser.save(done);
+        });
+
+        it("user instance", done => {
+            let user = User.findOne({name: sampleUser.username});
+            user
+                .remove()
+                .then(() => {
+                    return User.findOne({name: sampleUser.username});
+                })
+                .then(u => {
+                    assert(u === null);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
-    it("read created user", done => {
-      let user = User.findOne({ username: sampleUser.username })
-        .then(users => {
-          assert(
-            users.username === sampleUser.username,
-            "username dos not match"
-          );
-          assert(users.email === sampleUser.email, "Email does not match");
-          // console.log(`FFF: ${JSON.stringify(users)}`);
-          assert(users.enabled === true);
-          users.checkPassword(sampleUser.password, function(err, res) {
-            assert(res === true, "Password does not match");
-          });
-          done();
-        })
-        .catch(err => {
-          done(err);
+    describe("Update", () => {
+        it("update user password", done => {
+            let newPass = "321";
+            User.findOne({name: sampleUser.name}).then(user => {
+
+                expect(user).to.not.equal(null);
+                return user;
+
+            }).then(user => {
+                user.password = newPass;
+                user.save().then(() => {
+                    return User.findOne({name: sampleUser.name});
+                }).then(nuser => {
+                    nuser.checkPassword(newPass, function (err, isMatch) {
+                        expect(isMatch).to.equal(true);
+                        done();
+                    });
+                }).catch(e => {
+                    done(e);
+                });
+            }).catch(e => {
+                done(e);
+            });
+        });
+        it("update user email", done => {
+            let email = "321@example.com";
+            User.findOne({name: sampleUser.name}).then(user => {
+
+                expect(user).to.not.equal(null);
+                return user;
+
+            }).then(user => {
+                user.email = email;
+                user.save().then(() => {
+                    return User.findOne({name: sampleUser.name});
+                }).then(nuser => {
+                   expect(nuser.email).to.equal(email);
+                   done();
+                }).catch(e => {
+                    done(e);
+                });
+            }).catch(e => {
+                done(e);
+            });
+        });
+        it("check for incorrect password", done => {
+            let newPass = "321";
+            let wrongPass = "123";
+            User.findOne({name: sampleUser.name}).then(user => {
+
+                expect(user).to.not.equal(null);
+                return user;
+
+            }).then(user => {
+                user.password = newPass;
+                user.save().then(() => {
+                    return User.findOne({name: sampleUser.name});
+                }).then(nuser => {
+                    nuser.checkPassword(wrongPass, function (err, isMatch) {
+                        expect(isMatch).to.equal(false);
+                        done();
+                    });
+                }).catch(e => {
+                    done(e);
+                });
+            }).catch(e => {
+                done(e);
+            });
         });
     });
-  });
-
-  describe("Remove", () => {
-    beforeEach(done => {
-      var nuser = new User(sampleUser);
-      nuser.save(done);
-    });
-
-    it("user instance", done => {
-      let user = User.findOne({ name: sampleUser.username });
-      user
-        .remove()
-        .then(() => {
-          return User.findOne({ name: sampleUser.username });
-        })
-        .then(u => {
-          assert(u === null);
-          done();
-        })
-        .catch(err => {
-          done(err);
-        });
-    });
-  });
-
-  describe("Update", () => {
-    xit("update user", done => {
-      let user = User.find({ name: sampleUser.name }).then(users => {
-        console.log(users);
-      });
-
-      done();
-    });
-  });
 });
