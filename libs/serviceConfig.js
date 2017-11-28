8d725e80a31de66cf93a973df7845eabb0863766
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2017-11-28 21:00:27 +0000

    up

--- libs/serviceConfig.js
+++ libs/serviceConfig.js
@@ -1,19 +1,20 @@
 const file = require('fs');
 const nconf = require('nconf');
 const konsole = require('./konsole');
-function Config(){
+
+function Config() {
 
     nconf.argv().env("_");
     const environment = nconf.get("NODE:ENV") || "development";
-    konsole('NODE:ENV set to: '+environment,true);
-    nconf.file(environment,__dirname+"/../etc/"+environment+".json");
-    nconf.file("default", __dirname+"/../etc/default.json");
+    konsole('NODE:ENV set to: ' + environment, true);
+    nconf.file(environment, __dirname + "/../etc/" + environment + ".json");
+    nconf.file("default", __dirname + "/../etc/default.json");
 
 
 }
 
 Config.prototype.get = function (key) {
-   return nconf.get(key);
+    return nconf.get(key);
 };
 
 module.exports = new Config();
