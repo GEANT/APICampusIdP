f0278978055a465e8a25d27a3d897c430ae93296
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2018-08-01 11:39:47 +0100

    up

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,56 @@ const konsole = require('../libs/konsole');
 const verifyToken = require('../libs/verifyToken');
 const errPrefix = "255";
 const ExpressBrute = require('express-brute');
-
+const validator = require('validator');
 // store for dev only
 const store = new ExpressBrute.MemoryStore();
 const bruteforce = new ExpressBrute(store);
 const validateRegisterInput = require('../libs/validations/registerUser');
 
+/**
+ * @todo user acls
+ */
 /* GET users listing. */
 router.get('/', verifyToken,function (req, res, next) {
     const errors = {};
-    User.find().select(["username","email", "enabled"]).then(users => {
+    let filterQuery = {};
+    let queryByEmail = req.query.email;
+    if(queryByEmail !== 'undefined' && !_.isEmpty(queryByEmail)){
+        if(validator.isEmail(queryByEmail)){
+            filterQuery.email = queryByEmail;
+        }
+        else {
+            return res.status(400).json({error: true, message: "invalid query value"});
+        }
+    }
+
+    if(req.query.username !== 'undefined' && !_.isEmpty(req.query.username)){
+       filterQuery.username =  req.query.username;
+    }
+
+    User.find(filterQuery).select(["username","email", "enabled"]).then(users => {
         res.send(users);
     }).catch(err => {
         res.send('test respond with a resource: error');
     });
 
+});
+
+router.get('/:id', verifyToken, function(req,res,next){
+   let id =  req.params.id;
+   User.findOne({_id: id}).then( user => {
+       if(user){
+           res.json(user);
+
+       }
+       else {
+            res.status(404).json({error: true, message: "Not found"});
+
+       }
+   }).catch((err)=>{
+       konsole(err);
+       res.status(404).json({error: true, message: "Not found"});
+   });
 });
 
 /* register new user */
