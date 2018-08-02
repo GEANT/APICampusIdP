8d84f20fa9bf97e09fcd92caeff8da1dfdf8d62f
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2018-08-02 22:18:14 +0100

    up

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ const bodyParser = require('body-parser');
 const yaml = require('write-yaml');
 const passport = require('passport');
 const jsonld = require('jsonld');
+const nconf = require('nconf');
 const rfs = require('rotating-file-stream');
 const logDirectory = path.join(__dirname, 'logs');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-var accessLogStream = rfs('access.log', {
+const accessLogStream = rfs('access.log', {
     interval: '1d', // rotate daily
     path: logDirectory
 });
@@ -39,6 +40,8 @@ const konsole = require('./libs/konsole');
  */
 const appConfig = require('./libs/serviceConfig');
 const configer = require('./libs/serviceConfig');
+
+
 const app = express();
 
 const useHttps = configer.get("forcehttps") || false;
@@ -50,7 +53,7 @@ let proxyPort = parseInt(configer.get("proxy:port"),10);
 let proxyProtocol = configer.get("proxy:protocol") || "https";
 const hostname = configer.get("hostname");
 if (behindProxy) {
-    if (proxyProtocol == 'http') {
+    if (proxyProtocol === 'http') {
         if (proxyPort !== 80) {
             app.set('baseurl', 'http://' + hostname + '/');
         }
@@ -77,25 +80,27 @@ else {
 }
 
 app.set('appConfig', appConfig);
-
-
-
-
-
-
 const db_settings = appConfig.get('database');
-const db_uri = 'mongodb://' + db_settings.user + ':' + db_settings.pass + '@' + db_settings.host + ':' + db_settings.port + '/' + db_settings.dbname + '';
+const dbUser = nconf.get("DB:USER") || db_settings.user;
+const dbPass = nconf.get("DB:PASSWORD")||db_settings.pass;
+const dbHost = nconf.get("DB:HOST")||db_settings.host;
+const dbPort = nconf.get("DB:PORT")||db_settings.port;
+const dbName = nconf.get("DB:NAME")||db_settings.dbname;
+const db_uri = 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + ':' + dbPort + '/' + dbName + '';
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var version = require('mongoose-version');
+const version = require('mongoose-version');
 if(process.env.NODE_ENV !== 'test') {
     mongoose.connect(db_uri, {
         useMongoClient: true,
         /* other options */
+    }).then().catch(err => {
+
+        console.log('ZZZZZ');
     });
-};
+}
 
 
 
@@ -140,7 +145,7 @@ app.use('/schema', apiSchema);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
