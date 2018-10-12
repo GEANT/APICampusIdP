2a6d1dcc580e7872a352839b1277ae15bc793588
Janusz Ulanowski <2247817+janul@users.noreply.github.com>
2018-10-12 11:03:56 +0100

    list states

--- routes/tasks/idp.js
+++ routes/tasks/idp.js
@@ -14,6 +14,63 @@ const eventsConfig = require('../../libs/eventsConfig').events;
 
 
 
+router.get('/:name', verifyToken, function(req,res){
+    let name = req.params.name;
+    let pPromise = Provider.findOne({name: name});
+    let username = res.locals.tokenDecoded.sub;
+    let uPromise = User.findOne({username: username});
+    Promise.all([pPromise, uPromise]).then(
+        (results) => {
+            let provider = results[0];
+            let user = results[1];
+            if (user === null) {
+                return res.status(401).json({"error": true, "message": "Not permitted", "id": errorPrefix + "007"});
+            }
+            if (provider !== null) {
+
+                /**
+                 * @todo improve permission
+                 * */
+                let isPermitted = provider.creator._id.equals(user._id);
+
+
+                if (isPermitted) {
+
+                    let jobs = Job.find({provider: provider}).populate('creator');
+                    jobs.then( (tasks) =>{
+                        let tasksList = [];
+                        if(tasks) {
+                            for(let i = 0; i < tasks.length; i++ ){
+                                tasksList.push({
+                                    name: tasks[i].name,
+                                    created: tasks[i].createdAt,
+                                    creator: tasks[i].creator.username,
+                                    states: tasks[i].states
+                                })
+                            }
+                        }
+                        return res.json(tasksList);
+                    } )
+
+                }
+                else {
+                    return res.status(401).json({
+                        "error": true,
+                        "message": "Authorization failed-",
+                        "id": errorPrefix + "008"
+                    });
+                }
+
+
+            }
+            else {
+                return res.status(404).json({"error": true, "message": "Not found", "id": errorPrefix + "010"});
+            }
+        }
+    ).catch(err => {
+        return res.send(err);
+    });
+});
 
 router.post('/:name', verifyToken, function (req, res) {
 
